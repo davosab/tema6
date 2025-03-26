6d968dd3c9f516cb108e4f48ccaf1594bc39e649
davosab <davidsabau72@gmail.com>
2025-03-26 21:51:41 +0200

    house door and roof

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
 }
 
 function draw() {
-  background("#bbb");
+  // sky
+  background("#7edbed");
+  // grass
+  fill("#28c752");
+  stroke("#168a35");
+  rect(0, 280, 800, 120);
+  // house
+  stroke("black");
+  fill("red");
+  rect(300, 150, 200, 160);
+  // chimney
+  fill("gray");
+  rect(460, 60, 30, 90);
+  // roof
+  fill("brown");
+  beginShape();
+
+  vertex(280, 150);
+  vertex(350, 80);
+  vertex(450, 80);
+  vertex(520, 150);
+
+  endShape();
+  // door
+  fill("white");
+  rect(330, 210, 60, 100);
+  fill("#e3fafa");
+  circle(360, 240, 24);
 }
